d79f28d9b6f518d6d6b2ea833d272d2ac01c7eb8
xITEnginex92 <emina.saric@gmail.com>
2014-05-29 08:10:49 +0200

    commit1

---
+++ hogwarts_login/MainWindow.xaml.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace hogwarts_login
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void OnMouseLeftButtonDown(object sender, System.Windows.Input.MouseButtonEventArgs e)
+        {
+        	this.DragMove();
+        }
+
+        private void esc_Click(object sender, System.Windows.RoutedEventArgs e)
+        {
+        	this.Close();
+		}
+        private void button_gry_Click(object sender, System.Windows.RoutedEventArgs e)
+        {
+        	hogwarts_login_2 login2=new hogwarts_login_2("Gryffindor");
+            login2.Show();
+            this.Close();
+        }
+		private void button_rav_Click(object sender, System.Windows.RoutedEventArgs e)
+        {
+        	hogwarts_login_2 login2=new hogwarts_login_2("Ravenclaw");
+            login2.Show();
+            this.Close();
+        }
+		private void button_huff_Click(object sender, System.Windows.RoutedEventArgs e)
+        {
+        	hogwarts_login_2 login2=new hogwarts_login_2("Hufflepuff");
+            login2.Show();
+            this.Close();
+        }
+		private void button_sly_Click(object sender, System.Windows.RoutedEventArgs e)
+        {
+        	hogwarts_login_2 login2=new hogwarts_login_2("Slytherin");
+            login2.Show();
+            this.Close();
+        }
+    }
+}
